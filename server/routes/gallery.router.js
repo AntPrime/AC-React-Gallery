1e76da0f86f01518feb9887f4e87990359a280a5
Anthony Clarke <anthonydeandreclarke@gmail.com>
2024-11-23 13:20:45 -0600

    Get works and is pulling data from the database

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool')
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  //code here
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
+  const sqlText = `
+  SELECT * FROM "gallery";
+`
+pool.query(sqlText)
+  .then((Result) => {
+      console.log(`Result.rows is:`, Result.rows);
+      res.send(Result.rows);
+  })
+  .catch((error) => {
+      console.log(`dbError making database query ${sqlText}`, error);
+      res.sendStatus(500);
+  })
 });
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,43 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 function App() {
-    return (
-      <div>
+
+  const [galleryList, setGalleryList] = useState([])
+
+  useEffect(()=>{
+    fetchGallery();},[])
+
+  const fetchGallery = () =>{
+    console.log("fetching..")
+
+    axios({
+      method: "GET",
+      url: "/api/gallery"
+  })
+  .then((response) => {
+      console.log("Response: ", response.data)
+      setGalleryList(response.data)
+  })
+  .catch((err) => {
+      console.log("GET /api/gallery is broken")
+  })
+  }
+
+    return (<>
+      <div data-testid="app">
         <header>
           <h1>React Gallery</h1>
         </header>
 
-        <p>The gallery goes here!</p>
+        <p data-testid="galleryList"> What goes here?</p>
+        
         <img src="images/goat_small.jpg"/>
         <img src="images/goat_stache.png"/>
       </div>
-    );
+      </> );
 }
 
 export default App;
