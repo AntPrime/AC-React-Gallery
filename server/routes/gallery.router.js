af54b9e8de4e306c9c682f737f96c9607f7ca64d
Anthony Clarke <anthonydeandreclarke@gmail.com>
2024-11-24 11:19:51 -0600

    not sure what I just changed but the PUT seems to be updating the DB

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -5,54 +5,27 @@ const pool = require('../modules/pool')
 // PUT /gallery/like/:id
 
 router.put('/like/:id', (req, res) => {
-  console.log('Received PUT request for likes update.');
-
-  // Make sure the body contains the 'likes' field
-  if (!req.body.likes) {
-    return res.status(400).send('Likes value is required');
-  }
-
-  const likes = parseInt(req.body.likes);
-  
-  if (isNaN(likes)) {
-    return res.status(400).send('Likes value must be a number');
-  }
+  // !CODE HERE
+  console.log( '/ in Router PUT' );
+  console.log('req.params.id: ', req.params.id)
+  console.log(req.body)
 
   const queryText = `UPDATE "gallery" SET "likes"=$1 WHERE "id"=$2;`;
-  const values = [likes, req.params.id];
 
-  // Debug: Log the query and parameters
-  console.log('Query:', queryText);
-  console.log('Values:', values);
+  const likes = req.body.likes
+  const id = req.params.id
+  const values = [likes, id];
 
-  pool.query(queryText, values)
-    .then((results) => {
-      console.log('Update successful:', results);
-      res.sendStatus(200); // "OK"
-    })
-    .catch((err) => {
-      console.error('Error during database query:', err);
-      res.sendStatus(400); // Bad Request if query fails
-    });
+  console.log(likes)
+  pool.query( queryText, values ).then( ( results )=>{
+    console.log('Update Successful:', results)
+      res.sendStatus( 200 ); // "OK"
+  }).catch( ( err )=>{
+      // handle any errors
+      console.log( err );
+      res.sendStatus( 400 );
+  })
 });
-// router.put('/likes/:id', (req, res) => {
-//   // !CODE HERE
-//   console.log( '/ PUT:' );
-//   const queryText = `UPDATE "gallery" SET "likes"=$1 WHERE "id"=$2;`;
-
-//   const likes = req.body.likes
-//   const values = [likes, req.params.id];
-// console.log(req.body)
-//   console.log(likes)
-//   pool.query( queryText, values ).then( ( results )=>{
-//     console.log('Update Successful:', results)
-//       res.sendStatus( 200 ); // "OK"
-//   }).catch( ( err )=>{
-//       // handle any errors
-//       console.log( err );
-//       res.sendStatus( 400 );
-//   })
-// });
 
 // GET /gallery
 router.get('/', (req, res) => {
